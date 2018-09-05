478c20e439fadd3d7e2acabd430ff4a709de90e9
FlyRubble <2245117699@qq.com>
2018-09-05 21:16:46 +0800

    添加状态机

---
+++ Assets/Src/Framework/State/State.cs
@@ -0,0 +1,66 @@
+﻿namespace Framework
+{
+    /// <summary>
+    /// 状态
+    /// </summary>
+    public abstract class State
+    {
+        #region Variable
+        /// <summary>
+        /// 参数
+        /// </summary>
+        protected Param m_param;
+        #endregion
+
+        #region Variable
+        /// <summary>
+        /// 得到类型名
+        /// </summary>
+        public virtual string getName
+        {
+            get
+            {
+                return this.GetType().Name;
+            }
+        }
+        #endregion
+
+        #region Function
+        /// <summary>
+        /// 进入状态
+        /// </summary>
+        /// <param name="param"></param>
+        public virtual void OnEnter(Param param = null)
+        {
+            Param.Destroy(m_param);
+            m_param = param;
+            if (null == m_param)
+            {
+                m_param = Param.Create();
+            }
+        }
+
+        /// <summary>
+        /// 更新状态
+        /// </summary>
+        public virtual void OnUpdate()
+        {
+
+        }
+
+        /// <summary>
+        /// 退出状态
+        /// </summary>
+        /// <param name="param"></param>
+        public virtual void OnExit(Param param = null)
+        {
+            Param.Destroy(m_param);
+            m_param = param;
+            if (null == m_param)
+            {
+                m_param = Param.Create();
+            }
+        }
+        #endregion
+    }
+}
---
+++ Assets/Src/Framework/State/StateMachine.cs
@@ -0,0 +1,54 @@
+﻿namespace Framework
+{
+    using Singleton;
+    public class StateMachine : Singleton<StateMachine>
+    {
+        #region Variable
+        /// <summary>
+        /// 当前状态
+        /// </summary>
+        private State m_current = null;
+        #endregion
+
+        #region Property
+        /// <summary>
+        /// 得到当前状态
+        /// </summary>
+        public State current
+        {
+            get
+            {
+                return m_current;
+            }
+        }
+        #endregion
+
+        #region Function
+        /// <summary>
+        /// 进入状态
+        /// </summary>
+        /// <param name="state"></param>
+        /// <param name="param"></param>
+        public void OnEnter(State state, Param param = null)
+        {
+            if (m_current != null)
+            {
+                m_current.OnExit(param);
+            }
+            m_current = state;
+            if (m_current != null)
+            {
+                m_current.OnEnter(param);
+            }
+        }
+
+        /// <summary>
+        /// 更新状态
+        /// </summary>
+        public void OnUpdate()
+        {
+            m_current.OnUpdate();
+        }
+        #endregion
+    }
+}
