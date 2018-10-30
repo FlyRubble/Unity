0711df8a6cafe842ad70f1cb7ac810207a32ca81
FlyRubble <2245117699@qq.com>
2018-10-30 19:05:16 +0800

    exceltools

---
+++ ExcelTools/ExcelTools/Program.cs
@@ -0,0 +1,31 @@
+﻿using Excel;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace ExcelTools
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Setting.Init();
+
+            var a = Util.Open("d://char-角色配置表.xlsm", null);
+            Encoding cd = new UTF8Encoding(false);
+            string json = JsonConvert.SerializeObject(a, Formatting.Indented);
+            using (FileStream file = new FileStream("d://char.json", FileMode.Create, FileAccess.Write))
+            {
+                using (TextWriter writer = new StreamWriter(file, cd))
+                {
+                    writer.Write(json);
+                }
+            }
+        }
+    }
+}
